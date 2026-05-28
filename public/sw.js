10d1abafd9be1d0028427cc6623a6f8e73e6cf71
Ricard Penin Honrubia <ergoegos@gmail.com>
2026-05-28 13:24:15 +0200

    fix: add .nojekyll, remove stale eventos ref from service worker
    - Add .nojekyll to prevent Jekyll processing on GitHub Pages
    - Remove section-eventos.css from SW cache (file no longer exists)
    - Bump cache version to v40 to force SW update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-var CACHE = "rricajos-v39";
+var CACHE = "rricajos-v40";
 var ASSETS = [
   "./",
   "./index.html",
@@ -18,7 +18,6 @@ var ASSETS = [
   "./css/sections.css",
   "./css/section-services.css",
   "./css/section-portfolio.css",
-  "./css/section-eventos.css",
   "./css/section-contact.css",
   "./css/print.css",
   "./fonts/poppins-400-latin.woff2",
